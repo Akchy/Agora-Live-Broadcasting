4a8ab7a4caf176f1287987ecc759edb87fa2473a
Akchy <akarshashok12@gmail.com>
2020-05-16 12:43:13 +0530

    Added new UI

---
+++ android/app/src/main/kotlin/com/davish/agorartm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.davish.agorartm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,24 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore: unused_import
+import 'dart:ui';
+
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(PluginRegistry registry) {
+  FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
+  FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
+  FluttertoastWebPlugin.registerWith(registry.registrarFor(FluttertoastWebPlugin));
+  GoogleSignInPlugin.registerWith(registry.registrarFor(GoogleSignInPlugin));
+  SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
+  registry.registerMessageHandler();
+}
